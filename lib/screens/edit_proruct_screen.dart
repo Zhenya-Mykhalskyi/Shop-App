92f0426f9e82a2b6d32f5869c1c4874ab1e2899d
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-11-03 21:42:04 +0200

    add UserProductsScreen and EditProductScreen

---
+++ lib/screens/edit_proruct_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  //Данними управляємо тільки в цьому віджеті
+
+  static const routeName = '/edit-product';
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
