8a473b3016dd68b4539e0c218c1798c87b4a1912
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-11-11 18:04:22 +0200

    252:  Utilize optimistic uploading

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -195,7 +195,20 @@ class Products with ChangeNotifier {
   }
 
   void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+    final url = Uri.parse(
+        'https://exotic-fruits-shop-default-rtdb.europe-west1.firebasedatabase.app/products/$id.json');
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+
+    http.delete(url).then((response) {
+      if (response.statusCode >= 400) {}
+
+      existingProduct = null;
+    }).catchError((_) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+    });
+    _items.removeAt(existingProductIndex);
     notifyListeners();
   }
 }
