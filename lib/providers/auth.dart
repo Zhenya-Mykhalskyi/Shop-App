7aed581f6ab09c81ee6d3190da20f425dee575b5
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-11-17 21:18:35 +0200

    feat logIn logic and refactor Auth

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,9 +7,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD7PxeL5QloiEJ9U-21E5i8m3G81N5nYZk');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyD7PxeL5QloiEJ9U-21E5i8m3G81N5nYZk');
     final response = await http.post(
       url,
       body: json.encode(
@@ -21,5 +22,13 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
