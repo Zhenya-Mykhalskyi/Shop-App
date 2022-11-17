dfc84f32fa7be5888d8f4b3dc60376cbda361fba
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-11-17 19:04:49 +0200

    264: Add the AuthScreen and change homeroute in Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
@@ -10,6 +9,7 @@ import '../screens/orders_screen.dart';
 import '../providers/ordrers.dart';
 import '../screens/user_products_screen.dart';
 import '../screens/edit_proruct_screen.dart';
+import 'package:shop_app/screens/auth_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.lightGreen)
               .copyWith(secondary: Colors.green),
         ),
-        home: const ProductsOverviewScreen(),
+        home: const AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
           CartScreen.routeName: (ctx) => const CartScreen(),
