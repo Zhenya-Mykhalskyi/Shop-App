18fc372555c8c1c21592f6d4fe904d8a7f1927cf
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-11-19 16:52:59 +0200

    276: feat autoLogout in Auth and AppDrawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 import '../screens/user_products_screen.dart';
 import '../screens/orders_screen.dart';
+import '../providers/auth.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key key}) : super(key: key);
@@ -52,6 +54,15 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(UserProductsScreen.routeName);
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pop();
+              Provider.of<Auth>(context, listen: false).logout();
             },
           ),
         ],
