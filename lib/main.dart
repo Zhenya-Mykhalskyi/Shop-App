d7f7bf2c4381b698baf2e32e4568af54672da00a
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-11-19 16:32:48 +0200

    274: attach orders to users

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,9 @@ class MyApp extends StatelessWidget {
         ), //proxy для того, щоб управляти токеном з файлу Auth. При зміні Auth буде створений новий об'єкт Products
         ChangeNotifierProvider(create: (ctx) => Cart()),
         ChangeNotifierProxyProvider<Auth, Orders>(
-            update: (ctx, auth, previousOrders) => Orders(auth.token,
+            update: (ctx, auth, previousOrders) => Orders(
+                auth.token,
+                auth.userId,
                 previousOrders == null ? [] : previousOrders.orders)),
       ],
       child: Consumer<Auth>(
--- lib/providers/ordrers.dart
+++ lib/providers/ordrers.dart
@@ -21,8 +21,9 @@ class OrderItem {
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
   final String authToken;
+  final String userId;
 
-  Orders(this.authToken, param1);
+  Orders(this.authToken, this.userId, param1);
 
   List<OrderItem> get orders {
     return [..._orders];
@@ -30,7 +31,7 @@ class Orders with ChangeNotifier {
 
   Future<void> fetchAndSetOrders() async {
     final url = Uri.parse(
-        'https://exotic-fruits-shop-default-rtdb.europe-west1.firebasedatabase.app/orders.json?auth=$authToken');
+        'https://exotic-fruits-shop-default-rtdb.europe-west1.firebasedatabase.app/orders/$userId.json?auth=$authToken');
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -63,7 +64,7 @@ class Orders with ChangeNotifier {
 
   void addOrder(List<CartItem> cartProducts, double total) async {
     final url = Uri.parse(
-        'https://exotic-fruits-shop-default-rtdb.europe-west1.firebasedatabase.app/orders.json?auth=$authToken');
+        'https://exotic-fruits-shop-default-rtdb.europe-west1.firebasedatabase.app/orders/$userId.json?auth=$authToken');
     final timestamp = DateTime.now();
     final response = await http.post(
       url,
