fb332c379f3e3bc2aa4ade8217f81f778ca2225c
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-11-21 13:41:50 +0200

    add form container animation in AuthScreen via AnimatedBuilder

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -96,7 +96,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   final Map<String, String> _authData = {
@@ -105,6 +106,36 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  var containerHeight = 260;
+  AnimationController _controller;
+  Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      // аргумент, який спостерігає за конкретним віджетом, що відображається на екрані. Це оптимізує додаток
+      duration: const Duration(milliseconds: 300),
+    );
+    _heightAnimation = Tween<Size>(
+            begin: const Size(double.infinity, 260),
+            end: const Size(double.infinity, 320))
+        .animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linear,
+      ),
+    );
+    // _heightAnimation.addListener(() =>
+    //     setState(() {})); //перезапуск методу зборки, щоб перемалювати екран
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose(); //to clear the listener
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -178,10 +209,13 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _controller.forward(); //start animation
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller.reverse();
+      //коли змінюються моди, то контроллер анімаії вертається до попереднього значення
     }
   }
 
@@ -193,12 +227,17 @@ class _AuthCardState extends State<AuthCard> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 14.0,
-      child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: const EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+          // height: _authMode == AuthMode.Signup ? 320 : 260,
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: const EdgeInsets.all(16.0),
+          child: ch,
+          // анімуються тільки контейнер, а форма - ні
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -274,7 +313,6 @@ class _AuthCardState extends State<AuthCard> {
                     ),
                     child:
                         Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
-
                     // shape: RoundedRectangleBorder(
                     //   borderRadius: BorderRadius.circular(30),
                     // ),
