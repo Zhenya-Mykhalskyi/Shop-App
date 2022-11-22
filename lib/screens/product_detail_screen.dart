3b4dbc6dd219b066ebb31092d005d7c693fa56ef
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-11-22 15:11:55 +0200

    animate ProductDatailScreen via slivers

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -16,42 +16,91 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final deviceSize = MediaQuery.of(context).size;
     final loadedProduct =
         Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-      appBar: MyAppBar(
-        title: loadedProduct.title,
-        actions: Container(),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: 300,
-              child: Hero(
-                tag: loadedProduct.id,
-                child: Image.network(loadedProduct.imageUrl),
-              ),
+      // appBar: MyAppBar(
+      //   title: loadedProduct.title,
+      //   actions: Container(),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            iconTheme: const IconThemeData(color: Colors.white),
+            flexibleSpace: Stack(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://cdn.shopify.com/s/files/1/0512/2542/8161/products/mothers-day-selection-box-exoticfruitscouk-192513_1024x1024@2x.jpg?v=1645776779'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Positioned.fill(
+                  child: Opacity(
+                    opacity: 0.6,
+                    child: Container(
+                      color: const Color(0xFF000000),
+                    ),
+                  ),
+                ),
+                FlexibleSpaceBar(
+                  title: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    color: Theme.of(context).colorScheme.background,
+                    child: Text(
+                      loadedProduct.title,
+                      style: TextStyle(
+                          color: const Color.fromARGB(255, 34, 34, 34),
+                          backgroundColor:
+                              Theme.of(context).colorScheme.background,
+                          fontFamily: 'Anton'),
+                    ),
+                  ),
+                  background: Hero(
+                    tag: loadedProduct.id,
+                    child: Image.network(
+                      loadedProduct.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 10),
-            Text(
-              '\$${loadedProduct.price}',
-              style: const TextStyle(
-                  color: Color.fromARGB(255, 82, 211, 2), fontSize: 20),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                loadedProduct.description,
+            expandedHeight: deviceSize.height / 2.3,
+            pinned: true,
+            //appBar always be visible when we scroll
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const SizedBox(height: 10),
+              Text(
+                '\$${loadedProduct.price}',
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 82, 211, 2),
+                  fontSize: 20,
+                ),
                 textAlign: TextAlign.center,
-                softWrap: true,
               ),
-            )
-          ],
-        ),
+              const SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  loadedProduct.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              ),
+              const SizedBox(
+                height: 1000,
+              )
+            ]),
+          ),
+        ],
       ),
     );
   }
